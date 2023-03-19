290dda16d4db4953a137cccb2bdbe2d02c63f5dd
Debajyoti Dasgupta <debajyotidasgupta6@gmail.com>
2023-03-20 02:10:53 +0530

    Bug Fix 🪲

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import {
-  Route,
-  Routes,
-  BrowserRouter,
-} from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import { Route, Routes, HashRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router basename="/">
       <div className="App">
         <Navbar />
         <Routes>
           <Route path="/" exact element={<Home />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </Router>
   );
 }
-
 
 export default App;
